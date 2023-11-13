d53e2c2cc1b5d8fd8de20607239e1a4f524561bf
paradoxuum <notparadox04@gmail.com>
2023-11-13 19:47:45 +0000

    refactor: remove interface stories

--- src/client/interface/stories/interface/ReactiveButton.story.tsx
+++
@@ -1,9 +0,0 @@
-import Roact from "@rbxts/roact";
-import { ReactiveButton } from "../../components/interface/ReactiveButton";
-import { palette } from "../../constants/palette";
-import { story } from "../../util/story";
-
-export = story({
-	summary: "A reactive button",
-	story: () => <ReactiveButton size={UDim2.fromOffset(200, 50)} backgroundColor={palette.mantle} />,
-});
--- src/client/interface/stories/interface/TextField.story.tsx
+++
@@ -1,9 +0,0 @@
-import Roact from "@rbxts/roact";
-import { TextField } from "../../components/interface/TextField";
-import { story } from "../../util/story";
-
-export = story({
-	summary: "A text field component",
-	story: () => <TextField size={UDim2.fromOffset(100, 50)} />,
-	controls: {},
-});
