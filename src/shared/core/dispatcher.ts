be34595a61fa26dd98fe26de10d6d5f93d0f84de
paradoxuum <notparadox04@gmail.com>
2023-11-13 20:28:49 +0000

    feat: add default interaction reply

--- src/shared/core/dispatcher.ts
+++ src/shared/core/dispatcher.ts
@@ -3,6 +3,8 @@ import { CommandInteraction } from "./interaction";
 import { CommandPath } from "./path";
 import { BaseRegistry } from "./registry";
 
+const DEFAULT_REPLY_TEXT = "Command executed.";
+
 export abstract class BaseDispatcher {
 	constructor(private readonly registry: BaseRegistry) {}
 
@@ -14,6 +16,11 @@ export abstract class BaseDispatcher {
 		return Promise.try(() => {
 			const interaction = new CommandInteraction(Players.LocalPlayer, text);
 			command.execute(interaction, args);
+
+			if (!interaction.isReplyReceived()) {
+				interaction.reply(DEFAULT_REPLY_TEXT);
+			}
+
 			return interaction;
 		}).catch((err) => {
 			warn(`An error occurred while executing ${path}: ${err}`);
