860aa58f18b75b52d7b5ebd61152085af0d832a8
paradoxuum <notparadox04@gmail.com>
2023-11-13 05:33:35 +0000

    feat: implement dispatcher history

--- src/client/dispatcher.ts
+++ src/client/dispatcher.ts
@@ -2,13 +2,37 @@ import { Players } from "@rbxts/services";
 import { CommandPath } from "../shared";
 import { BaseDispatcher } from "../shared/core/dispatcher";
 import { ClientRegistry } from "./registry";
+import { HistoryEntry } from "./types";
 
 export class ClientDispatcher extends BaseDispatcher {
+	private readonly history: HistoryEntry[] = [];
+	private readonly historyEvent = new Instance("BindableEvent");
+
 	constructor(registry: ClientRegistry) {
 		super(registry);
 	}
 
 	async run(path: CommandPath, text: string) {
-		return this.executeCommand(path, Players.LocalPlayer, text);
+		const interaction = await this.executeCommand(path, Players.LocalPlayer, text);
+		if (!interaction.isReplyReceived()) {
+			return;
+		}
+
+		const entry: HistoryEntry = {
+			text: interaction.getReplyText()!,
+			success: interaction.isReplySuccess()!,
+			sentAt: interaction.getReplyTime()!,
+		};
+
+		this.history.push(entry);
+		this.historyEvent.Fire(entry);
+	}
+
+	getHistory() {
+		return this.history;
+	}
+
+	getHistorySignal() {
+		return this.historyEvent.Event;
 	}
 }
