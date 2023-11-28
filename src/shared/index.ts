7913697e0249111cf8ca3c26a63179e25c22daa5
paradoxuum <notparadox04@gmail.com>
2023-11-28 08:43:49 +0000

    feat: export base registry

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,5 +2,6 @@ export * from "./builtin";
 export * from "./core/decorators";
 export * from "./core/interaction";
 export * from "./core/path";
+export * from "./core/registry";
 export * from "./types";
 export * from "./util/type";
