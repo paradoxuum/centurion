a24f6549fb5186e19d915713272542f7ce125861
paradoxuum <notparadox04@gmail.com>
2023-12-01 17:24:53 +0000

    test: switch to midori, add and fix unit tests

---
+++ src/shared/util/string.test.ts
@@ -0,0 +1,38 @@
+import { TestProps } from "@rbxts/midori";
+import { endsWithSpace, formatPartsAsPath, splitStringBySpace } from "./string";
+
+export = (x: TestProps) => {
+	x.nested("split string", () => {
+		x.test("splits one space", () => {
+			const testString = "part1 part2 part3";
+			const parts = splitStringBySpace(testString);
+			x.assertEqual(parts.size(), 3);
+		});
+
+		x.test("splits multiple spaces", () => {
+			const testString = "part1   part2  part3    part4  ";
+			const parts = splitStringBySpace(testString);
+			x.assertEqual(parts.size(), 4);
+		});
+
+		x.test("takes into account quoted sentences", () => {
+			const testString = `part1 "part2 but quoted" part3 'part4' "part5"`;
+			const parts = splitStringBySpace(testString);
+			x.assertEqual(parts.size(), 5);
+			x.assertEqual(parts[1], "part2 but quoted");
+			x.assertEqual(parts[3], "part4");
+			x.assertEqual(parts[4], "part5");
+		});
+	});
+
+	x.test("determines if a string ends in a space", () => {
+		x.assertEqual(endsWithSpace("test"), false);
+		x.assertEqual(endsWithSpace("test   "), true);
+	});
+
+	x.test("formats string array as a path string", () => {
+		const pathString = "part1/part2/part3";
+		const parts = pathString.split("/");
+		x.assertEqual(formatPartsAsPath(parts), pathString);
+	});
+};
--- test/src/server/main.server.ts
+++ test/src/server/main.server.ts
@@ -1,5 +1,22 @@
 import { CommanderServer } from "@rbxts/commander";
+import { runTests } from "@rbxts/midori";
 
+// Run unit tests
+const getInstanceFromPath = (parts: string[]) => {
+	let last: Instance = game;
+	for (const part of parts) {
+		const partInstance = last.FindFirstChild(part);
+		if (partInstance === undefined) return undefined;
+		last = partInstance;
+	}
+	return last;
+};
+
+const [root, parts] = $getModuleTree("@rbxts/commander");
+const srcPath = getInstanceFromPath([root.ClassName, ...parts]);
+if (srcPath !== undefined) runTests(srcPath);
+
+// Start commander
 CommanderServer.start((registry) => {
 	const commandContainer = script.Parent!.WaitForChild("commands");
 	registry.registerCommandsIn(commandContainer);
