0508a956575b70700584675246b76ec26699c838
paradoxuum <notparadox04@gmail.com>
2024-02-14 13:12:20 +0000

    feat: rework client-server syncing

--- src/server/registry.ts
+++ src/server/registry.ts
@@ -6,7 +6,6 @@ import { Remotes, SyncData } from "../shared/network";
 import { ServerOptions } from "./types";
 
 export class ServerRegistry extends BaseRegistry {
-	private readonly syncMap = new Map<Player, CommandPath[]>();
 	private commandFilter: (path: CommandPath, player: Player) => boolean = () =>
 		true;
 
@@ -19,18 +18,7 @@ export class ServerRegistry extends BaseRegistry {
 		}
 
 		Remotes.SyncStart.OnServerEvent.Connect((player) => {
-			const pathArray: CommandPath[] = [];
-			for (const [_, command] of this.commands) {
-				if (!this.commandFilter(command.getPath(), player)) continue;
-				pathArray.push(command.getPath());
-			}
-			this.syncMap.set(player, pathArray);
-
 			Remotes.SyncDispatch.FireClient(player, this.getSyncData(player));
-		});
-
-		Players.PlayerRemoving.Connect((player) => {
-			this.syncMap.delete(player);
 		});
 	}
 
@@ -39,56 +27,23 @@ export class ServerRegistry extends BaseRegistry {
 		group?: CommandGroup | undefined,
 	) {
 		const command = super.registerCommand(data, group);
-
-		const path = command.getPath();
 		for (const player of Players.GetPlayers()) {
-			if (!this.commandFilter(path, player)) continue;
-			const pathArray = this.syncMap.get(player) ?? [];
-			pathArray.push(path);
-			this.syncMap.set(player, pathArray);
-
 			Remotes.SyncDispatch.FireClient(player, this.getSyncData(player));
 		}
-
 		return command;
 	}
 
 	private getSyncData(player: Player): SyncData {
-		const commandPaths = this.syncMap.get(player) ?? [];
-		const rootGroupNames: string[] = [];
-
 		const syncedCommands = new Map<string, CommandOptions>();
-		for (const path of commandPaths) {
-			const command = this.getCommand(path);
-			if (command === undefined) continue;
-
-			if (path.getSize() > 1) {
-				rootGroupNames.push(path.getRoot());
-			}
-
-			const readonlyArgs = command.options.arguments;
-			let args: ArgumentOptions[] | undefined;
-			if (readonlyArgs !== undefined) {
-				args = [];
-				for (const arg of readonlyArgs) {
-					args.push({
-						...arg,
-					});
-				}
-			}
+		for (const [pathString, command] of this.commands) {
+			const path = CommandPath.fromString(pathString);
+			if (!this.commandFilter(path, player)) continue;
 
 			syncedCommands.set(path.toString(), {
 				name: command.options.name,
-				description: command.options.description,
-				arguments: args,
+				aliases: command.options.aliases as string[] | undefined,
+				arguments: command.options.arguments as ArgumentOptions[] | undefined,
 			});
-		}
-
-		const rootGroups: CommandGroup[] = [];
-		for (const name of rootGroupNames) {
-			const group = this.groups.get(name);
-			if (group === undefined) continue;
-			rootGroups.push(group);
 		}
 
 		return {
