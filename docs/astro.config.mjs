1b42cf9756f91f3d84cc314aae423fc1494e9b22
paradox <hello@paradoxum.dev>
2024-06-02 21:13:39 +0100

    Migrate docs to starlight (#29)

---
+++ docs/astro.config.mjs
@@ -0,0 +1,59 @@
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "Commander",
+			description: "Flexible command framework for roblox-ts",
+			social: {
+				github: "https://github.com/paradoxuum/commander",
+				discord: "https://discord.roblox-ts.com/",
+			},
+			logo: {
+				dark: "src/assets/logo-dark.svg",
+				light: "src/assets/logo-light.svg",
+			},
+			sidebar: [
+				{
+					label: "Quick Start",
+					items: [
+						{
+							label: "Getting Started",
+							link: "getting-started",
+						},
+						{
+							label: "Examples",
+							link: "examples",
+						},
+					],
+				},
+				{
+					label: "Commands",
+					autogenerate: {
+						directory: "commands",
+					},
+				},
+				{
+					label: "Types",
+					autogenerate: {
+						directory: "types",
+					},
+				},
+				{
+					label: "Registration",
+					autogenerate: {
+						directory: "registration",
+					},
+				},
+			],
+			customCss: ["./src/tailwind.css", "./src/custom.css"],
+		}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+});
