978f2985b94aa9d69458056e1ec6ee59877930ab
paradoxuum <hello@paradoxum.dev>
2024-06-02 18:31:57 +0100

    docs: migrate to starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,55 @@
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "Commander",
+			description: "Flexible command framework for roblox-ts",
+			social: {
+				github: "https://github.com/paradoxuum/commander",
+				discord: "https://discord.roblox-ts.com/",
+			},
+			sidebar: [
+				{
+					label: "Quick Start",
+					items: [
+						{
+							label: "Getting Started",
+							link: "getting-started",
+						},
+						{
+							label: "Examples",
+							link: "examples",
+						},
+					],
+				},
+				{
+					label: "Commands",
+					autogenerate: {
+						directory: "commands",
+					},
+				},
+				{
+					label: "Types",
+					autogenerate: {
+						directory: "types",
+					},
+				},
+				{
+					label: "Registration",
+					autogenerate: {
+						directory: "registration",
+					},
+				},
+			],
+			customCss: ["./src/tailwind.css", "./src/custom.css"],
+		}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+});
---
+++ docs/tailwind.config.mjs
@@ -0,0 +1,19 @@
+import starlightPlugin from "@astrojs/starlight-tailwind";
+import colors from "tailwindcss/colors";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				accent: colors.emerald,
+				gray: colors.zinc,
+			},
+			fontFamily: {
+				mono: ['"JetBrains Mono"'],
+			},
+		},
+	},
+	plugins: [starlightPlugin()],
+};
