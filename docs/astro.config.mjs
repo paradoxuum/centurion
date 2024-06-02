2e07f16f336410c7b4ec638092b1a4a2c3103604
paradoxuum <hello@paradoxum.dev>
2024-06-02 21:37:59 +0100

    build: add deploy workflow for docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://paradoxuum.github.io",
+	base: "commander",
 	integrations: [
 		starlight({
 			title: "Commander",
