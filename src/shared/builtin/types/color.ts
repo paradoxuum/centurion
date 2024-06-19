4a836dbe2958bca37d3868bfd74b7139e37f0d3c
paradoxuum <hello@paradoxum.dev>
2024-06-19 01:49:02 +0100

    chore: fix formatting

--- src/shared/builtin/types/color.ts
+++ src/shared/builtin/types/color.ts
@@ -222,7 +222,9 @@ export const BrickColorType = TypeBuilder.create<BrickColor>(
 	.transform((text) => {
 		if (!brickColorNames.has(text))
 			return TransformResult.err("Invalid BrickColor");
-		return TransformResult.ok(new BrickColor(text as BrickColorsByNumber[keyof BrickColorsByNumber]));
+		return TransformResult.ok(
+			new BrickColor(text as BrickColorsByNumber[keyof BrickColorsByNumber]),
+		);
 	})
 	.suggestions(() => brickColorNameArray)
 	.build();
