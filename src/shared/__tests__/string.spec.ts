5a9a3e2297d3aa902e08256d9e4b0188fa2d0107
paradoxuum <hello@paradoxum.dev>
2024-06-19 17:10:47 +0100

    test: add unit tests for splitting strings by comma

--- src/shared/__tests__/string.spec.ts
+++ src/shared/__tests__/string.spec.ts
@@ -2,16 +2,20 @@ import { describe, expect, test } from "@rbxts/jest-globals";
 import { endsWithSpace, formatPartsAsPath, splitString } from "../util/string";
 
 describe("split string", () => {
-	test("splits one space", () => {
-		const testString = "part1 part2 part3";
-		const parts = splitString(testString, " ");
-		expect(parts.size()).toBe(3);
+	test("splits single characters", () => {
+		const spaceString = "part1 part2 part3";
+		expect(splitString(spaceString, " ").size()).toBe(3);
+
+		const commaString = "part1,part2,part3";
+		expect(splitString(commaString, ",").size()).toBe(3);
 	});
 
-	test("splits multiple spaces", () => {
-		const testString = "part1   part2  part3    part4  ";
-		const parts = splitString(testString, " ");
-		expect(parts.size()).toBe(4);
+	test("splits multiple characters", () => {
+		const spaceString = "part1   part2  part3  ";
+		expect(splitString(spaceString, " ").size()).toBe(3);
+
+		const commaString = "part1,,part2,,part3,,";
+		expect(splitString(commaString, ",").size()).toBe(3);
 	});
 
 	test("takes into account quoted sentences", () => {
