f303c0b51b580ff559207a271e62f4088145bb66
paradoxuum <hello@paradoxum.dev>
2024-07-20 06:35:05 +0100

    build: combine publish workflows into single workflow, check packages in order

--- .github/scripts/util.mjs
+++ .github/scripts/util.mjs
@@ -1,4 +1,52 @@
 import { execSync } from "node:child_process";
+import { existsSync, readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
+export function getChangedPackages(nightly) {
+	const packages = [];
+
+	if (isPackageChanged("packages/core", nightly)) {
+		packages.push("packages/core");
+	}
+
+	if (isPackageChanged("packages/ui", nightly)) {
+		packages.push("packages/ui");
+	}
+
+	return packages;
+}
+
+function isPackageChanged(directory, nightly) {
+	const packagePath = resolve(directory, "package.json");
+	if (!existsSync(packagePath)) {
+		throw `Could not find package.json at ${packagePath}`;
+	}
+
+	const packageJson = JSON.parse(readFileSync(packagePath, "utf8"));
+	const tag = nightly ? "next" : "latest";
+	const latest = execSync(`npm view ${packageJson.name} dist-tags.${tag}`)
+		.toString("utf8")
+		.trim();
+
+	if (nightly) {
+		const hash = latest.split("-").pop();
+		const result = execSync(
+			`git diff --quiet ${hash} HEAD -- ${directory} || echo changed`,
+		)
+			.toString("utf8")
+			.trim();
+
+		if (result === "changed") return true;
+	} else if (
+		packageJson.version.localeCompare(latest, undefined, {
+			numeric: true,
+		}) === 1
+	) {
+		return true;
+	}
+
+	return false;
+}
 
 export function getDevVersion(version) {
 	const [major, minor, patch] = version
